0543f924f5e06a1ab89d35961b7bc9ff7947b0c3
Admin <tmstudentler@gmail.com>
2021-08-11 19:00:20 +0300

    step 2

--- app/src/main/java/com/example/videochat/UI/MainActivity.java
+++ app/src/main/java/com/example/videochat/UI/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.videochat.UI;
 
 import static android.view.View.VISIBLE;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -13,6 +16,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.videochat.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     TextView txtRegister;
     Button btnLogin;
     ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +39,58 @@ public class MainActivity extends AppCompatActivity {
         txtRegister = findViewById(R.id.txtRegister);
         progressBar = findViewById(R.id.progressBar);
         btnLogin = findViewById(R.id.btnLogin);
+        mAuth = FirebaseAuth.getInstance();
 
         btnLogin.setOnClickListener(view -> {
             Toast.makeText(MainActivity.this, "The button is not ready yet!", Toast.LENGTH_SHORT).show();
         });
 
+        userLogin();
+
 
         clickText();
+    }
+
+    private void userLogin() {
+        btnLogin.setOnClickListener(view -> {
+            String email = editTextEmail.getText().toString().trim();
+            String password = editTextPass.getText().toString().trim();
+
+            if (email.isEmpty()){
+                editTextEmail.setError("Email is required!");
+                editTextEmail.requestFocus();
+                return;
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                editTextEmail.setError("Please enter a valid email!");
+                editTextPass.requestFocus();
+                return;
+            }
+
+            if (password.isEmpty()){
+                editTextPass.setError("Password is required!");
+                editTextPass.requestFocus();
+                return;
+            }
+
+            if (password.length() < 6){
+                editTextPass.setError("Please min 6 symbol");
+            }
+
+            progressBar.setVisibility(VISIBLE);
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful()){
+                      startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                    } else {
+                        Toast.makeText(MainActivity.this, "Failed to login!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        });
     }
 
 
